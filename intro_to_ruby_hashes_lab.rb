ac4c9e70673aedfd74960685f2b924f5c969a706
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-08 21:44:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,22 +4,22 @@ def new_hash
 end
 
 def my_hash
-  {:a => '1'}
+  { :a => '1' }
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  {:name => 'Grace Hopper'}
+  { :name => 'Grace Hopper' }
 end
 
 def id_generator
-  {:id => 1}
+  { :id => 1 }
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  {key => value}
+  { key => value }
 end
 
 def read_from_hash(hash, key)
